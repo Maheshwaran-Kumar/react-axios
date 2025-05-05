9112cc1e5dc6e7264994e0ee4d0c29a5851390ee
Maheshwaran-Kumar <maheshwarank9111@gmail.com>
2025-05-05 12:52:21 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserListComponent from "./user_list";
+import UserDetailComponent from "./user_detail";
+ 
+function App() {
+  return (
+<div className="App">
+<h2>Axios demo</h2>
+ 
+      <BrowserRouter>
+<Routes>
+<Route path="/" element={<UserListComponent />} />
+<Route path="/user/:id" element={<UserDetailComponent />} />
+</Routes>
+</BrowserRouter>
+</div>
+  );
+}
+ 
+export default App;
